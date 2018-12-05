c777546e23ca23a5ca32a14dd13e8b993d8b4fb6
Christine Ernst <christineernst@Christines-Air.fios-router.home>
2018-12-05 12:32:48 -0500

    Get authentication routes working and initialize basic components
    - Make additional modifications on the front end to prepare for
    successful signup and login
    - Write additional AJAX calls in store to sign up, log in and
    properly redirect once components are in place (not yet tested)
    - Get backend routes working (tested successfully with Postman)

--- client/index.js
+++ client/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+import { BrowserRouter as Router } from 'react-router-dom';
 import '../public/style.css';
+import history from './history';
 
 ReactDOM.render(
 	<Provider store={store}>
-		<div>Hello, world!</div>,
+		<Router history={history}>
+			<App />
+		</Router>
 	</Provider>,
 	document.getElementById('app')
 );
