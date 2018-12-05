6e46e66701fbc4ec64a06ea061144a0d958dbae9
Christine Ernst <christineernst@Christines-Air.fios-router.home>
2018-12-05 00:40:56 -0500

    Set up authentication
    - Hash and salt passwords in User model
    - Write sign up, login, logout and fetch user routes
    - Write up session middleware boilerplate

--- server/index.js
+++ server/index.js
@@ -3,11 +3,37 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const db = require('./db');
+const session = require('express-session');
+const passport = require('passport');
+
+passport.serializeUser((user, done) => {
+  try {
+    done(null, user.id);
+  } catch (err) {
+    done(err);
+  }
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => done(null, user))
+    .catch(done);
+});
 
 app.use(morgan('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET ||'Fedya was here',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/api', require('./apiRoutes')); 
 
@@ -24,6 +50,13 @@ app.use(function (err, req, res, next) {
 });
 
 const port = process.env.PORT || 3000; 
-app.listen(port, function () {
-  console.log(`Listening on port ${port}`);
-});
+
+async function start(){
+	await db.sync({force: true});  // sync our database
+    app.listen(port, function () {
+		console.log(`Listening on port ${port}`);
+	})
+}
+
+start();
+
--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,14 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
       }
-    ]
+    ],
   }
 }
