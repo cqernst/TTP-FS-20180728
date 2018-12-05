2208defb23798442a94a9f4a46534b93cb5ad4ed
Christine Ernst <christineernst@Christines-Air.fios-router.home>
2018-12-04 19:25:03 -0500

    Set up Express server
    - Include logging middleware
    - Include body parsing middleware
    - Set up separate folder for API routes, as expect there will be more than just users
    - Set server listening on port 3000

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api', require('./apiRoutes')); 
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use('*', function (req, res) {
+  res.sendFile(path.join(__dirname,'..', 'index.html'));
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+const port = process.env.PORT || 3000; 
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
+});
