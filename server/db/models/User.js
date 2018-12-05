652ce9f02579b310b8cf4aa006b58c4c502b2cf9
Christine Ernst <christineernst@Christines-Air.fios-router.home>
2018-12-04 22:31:19 -0500

    Set up and sync database
    - Create User model
    - Instantiate database with Sequelize
    - Sync DB before establishing Express server

---
+++ server/db/models/User.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const User = db.define('user', {
+  email: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    
+  }
+})
+
+module.exports = User
