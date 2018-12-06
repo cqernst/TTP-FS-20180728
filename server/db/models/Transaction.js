d302f5ec9a3af307424a1e749b299391a1e85301
Christine Ernst <christineernst@Christines-Air.fios-router.home>
2018-12-05 20:18:33 -0500

    Write transaction hook to calculate total number of shares a user has
    - write a helper method and beforeCreate hook in the Transaction model
    - In order to prevent having to query and filter the entire database
    of transactions to reset a user's total holdings of stock in a given
    company each time a transaction takes place, the hook locates the last
    entry where the user sold or purchased that company's stock. It then adds
    the number of shares being transacted in the current transaction and
    updates that entry's total figure.

--- server/db/models/Transaction.js
+++ server/db/models/Transaction.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const db = require('../db.js');
 
-const Transaction = db.define('lineItems', {
+const Transaction = db.define('transactions', {
 	id: {
 		type: Sequelize.INTEGER,
 		primaryKey: true,
@@ -11,18 +11,29 @@ const Transaction = db.define('lineItems', {
 		type: Sequelize.INTEGER,
 		allowNull: false,
 	},
+	total: {
+		type: Sequelize.INTEGER,
+	},
 });
 
 const getTotal = function(transaction) {
-	//find last transaction where the user purchased stock from this company
+	//find last transaction where the user sold or purchased stock from this company
 	let lastEntry = Transaction.findAll({
 		limit: 1,
 		where: {
-			userId: this.userId,
-			stockId: this.stockId,
+			userId: transaction.userId,
+			stockId: transaction.stockId,
 		},
 		order: [['createdAt', 'DESC']],
 	});
+
+	console.log('this is the last entry', lastEntry);
+
+	/*Sets current total number of stocks that the user has from the company
+	to the previous total plus or minus the amount being bought or sold in 
+	the given transaction*/
+
+	transaction.total = lastEntry[0].total + transaction.quantity;
 };
 
 Transaction.beforeCreate(getTotal);
